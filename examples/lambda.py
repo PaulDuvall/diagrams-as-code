57cc57fbe83b5bbe436628fff99587bcab7d0dce
“Paul <pduvall@gmail.com>
2020-06-12 18:34:32 +0000

    update Lambda diagram

--- examples/lambda.py
+++ examples/lambda.py
@@ -12,6 +12,11 @@ with Diagram("Running Lambda", show=False):
             handlers = [Lambda("GetData"),
                         Lambda("TestData"),
                         Lambda("proc3")]
+                        
+    with Cluster("Storage"):
+        handlers = [S3("Pipeline"),
+                    S3("Artifacts"),
+                    S3("Storage1")]
         
     store = S3("events store") 
     tbd = APIGateway("GetData") 
