76628ca689d498580cc3eccc6a88b98b866a2b11
“Paul <pduvall@gmail.com>
2020-06-12 18:26:13 +0000

    update Lambda diagram

--- examples/lambda.py
+++ examples/lambda.py
@@ -2,16 +2,18 @@ from diagrams import Cluster, Diagram
 from diagrams.aws.compute import Lambda
 from diagrams.aws.storage import S3
 from diagrams.aws.network import Route53
+from diagrams.aws.network import APIGateway
 
 with Diagram("Running Lambda", show=False):
     dns = Route53("dns")
 
     with Cluster("Event Flows"):
         with Cluster("Processing"):
-            handlers = [Lambda("proc1"),
-                        Lambda("proc2"),
+            handlers = [Lambda("GetData"),
+                        Lambda("TestData"),
                         Lambda("proc3")]
         
-    store = S3("events store")        
+    store = S3("events store") 
+    tbd = APIGateway("GetData") 
 
     handlers >> store
