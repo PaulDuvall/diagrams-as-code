5c6804fe00fda3c8ee0d36bcbaa47a0e66bb9c9b
“Paul <pduvall@gmail.com>
2020-06-15 13:19:36 +0000

    remove unused packages

--- examples/lambda.py
+++ examples/lambda.py
@@ -2,7 +2,6 @@
 from diagrams import Cluster, Diagram
 from diagrams.aws.compute import Lambda
 from diagrams.aws.storage import S3
-from diagrams.aws.network import Route53
 from diagrams.aws.network import APIGateway
 from diagrams.aws.database import DynamodbTable
 from diagrams.aws.security import IdentityAndAccessManagementIam
@@ -12,10 +11,6 @@ from diagrams.aws.devtools import Codedeploy
 from diagrams.aws.devtools import Codepipeline
 from diagrams.aws.management import Cloudformation
 from diagrams.aws.devtools import CommandLineInterface
-from diagrams.aws.compute import ECS
-from diagrams.aws.database import ElastiCache, RDS
-from diagrams.aws.network import ELB
-from diagrams.aws.network import Route53
 
 with Diagram("Serverless Web Apps", show=False, direction="TB"):
     
