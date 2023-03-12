bc91f866d5e589899bc44ca3972e8a9e77f68a3e
Paul Duvall <pduvall@gmail.com>
2023-03-12 14:44:12 +0000

    add eventbridge

---
+++ examples/app.py
@@ -0,0 +1,32 @@
+# https://diagrams.mingrammer.com/docs/nodes/aws
+from diagrams import Cluster, Diagram
+from diagrams.aws.compute import Lambda
+from diagrams.aws.storage import S3
+from diagrams.aws.network import APIGateway
+from diagrams.aws.database import DynamodbTable
+from diagrams.aws.security import IdentityAndAccessManagementIam
+from diagrams.aws.devtools import Codebuild
+from diagrams.aws.devtools import Codecommit
+from diagrams.aws.devtools import Codedeploy
+from diagrams.aws.devtools import Codepipeline
+from diagrams.aws.management import Cloudformation
+from diagrams.aws.devtools import CommandLineInterface
+from diagrams.aws.integration.Eventbridge Eventbridge
+
+with Diagram("finance-buxfer", show=False, direction="TB"):
+    
+
+    with Cluster("CloudFormation"):
+        cloudformation = Cloudformation("Stack")
+        cloudformation >> IdentityAndAccessManagementIam("IAM") >> Codecommit("CodeCommit") >> Codebuild("CodeBuild") >> S3("S3") >> Codepipeline("CodePipeline")
+
+    with Cluster("CodePipeline"):
+        codepipeline = Codepipeline("Pipeline")
+        codepipeline >> Codecommit("CodeCommit") >> Codebuild("CodeBuild") >> Cloudformation("CloudFormation")
+      
+    with Cluster("Serverless Application Model"):
+        sam = Cloudformation("SAM Template")
+        sam >> Lambda("Lambda") >> DynamodbTable("DynamoDB") >> Eventbridge("EventBridge Rule")
+        cloudformation >> codepipeline >> sam
+        
+
